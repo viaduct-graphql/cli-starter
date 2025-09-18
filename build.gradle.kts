78e6db25286a17dd8cdab1ebbdff6e6d8af02053
ViaBot <viabot@ductworks.io>
2025-09-18 11:45:26 +0000

    🐚 CLI Demo App
    GitOrigin-RevId: 2ec960ddb864d2fdefdd1aefdff3665c6419d499

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+    alias(libs.plugins.viaduct.application)
+    alias(libs.plugins.viaduct.module)
+    application
+}
+
+viaductApplication {
+    modulePackagePrefix.set("com.example.viadapp")
+}
+
+viaductModule {
+    modulePackageSuffix.set("resolvers")
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.10")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.9.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+}
+
+application {
+    mainClass.set("com.example.viadapp.ViaductApplicationKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "viaduct-cli-starter"
+
+val viaductVersion: String by settings
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            // This injects a dynamic value that your TOML can reference.
+            version("viaduct", viaductVersion)
+        }
+    }
+}
